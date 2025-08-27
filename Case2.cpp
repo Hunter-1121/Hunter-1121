0fda9527a75c43d0c9ae2204f1bd2edc10eb2827
Hunter 1121 <herobrine0613l@gmail.com>
2025-08-27 15:25:24 +0700

    Add files via upload

---
+++ Case2.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define elif else if
+
+signed main(){
+    fastio
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(auto &x : a){
+        cin >> x;
+    }
+    for(int j = 0; j < n; j++){
+        for(int i = 0; i < n - j - 1; i++){
+            if(a[i] > a[i + 1]){
+                swap(a[i], a[i + 1]);
+            }
+        }
+    }
+
+    for(auto x : a){
+        cout << x << ' ';
+    }
+}
